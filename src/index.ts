4badd5ed91ada1c0f80bff3b1aa3ca8b3069fd4e
Christian <blankj.me@gmail.com>
2024-07-15 19:55:03 +0200

    feat: basic mqtt subscribe & publish

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://test.mosquitto.org");
+
+client.on("connect", () => {
+  client.subscribe("presence", (err: any) => {
+    if (!err) {
+      client.publish("presence", "Hello mqtt");
+    }
+  });
+});
+
+client.on("message", (topic: any, message: any) => {
+  // message is Buffer
+  console.log(message.toString());
+  client.end();
+});
+
+
+
 
 const app = new Hono()
 
