14d52210d94f4114108cfd905c9be42685c521d9
Christian <blankj.me@gmail.com>
2024-11-12 10:56:17 +0100

    chore: changed tacticle sample data for more realistic data

--- src/guideService.ts
+++ src/guideService.ts
@@ -38,12 +38,12 @@ const images: ImageItem[] = [
     {
         id: 'e2_w2-2',
         url: `${baseUrl}e2/w2/w2-2.png`,
-        description: 'Der Orientierungspunkt befindet an der Wand, direkt vor dir.'
+        description: 'Der Orientierungspunkt befindet sich an der Wand, direkt vor dir.'
     },
     {
         id: 'e2_w2-3',
         url: `${baseUrl}e2/w2/w2-3.png`,
-        description: 'Der Orientierungspunkt links an der Wand des Zwischenflures.'
+        description: 'Der Orientierungspunkt befindet sich links an der Wand des Zwischenflures.'
     },
     {
         id: 'e2_w3-1',
@@ -83,44 +83,64 @@ const images: ImageItem[] = [
 ];
 const tactileLandmarks: TactileLandmark[] = [
     {
-        id: 'handrail_main',
-        description: 'Ein durchgehender Handlauf an der rechten Wand, aus glattem Metall, leicht gewärmt durch die Heizung darunter'
+        id: 'treppengelaender',
+        description: 'Durchgehendes Treppengeländer aus geriffeltem Edelstahl, leicht gekühlt, mit taktilen Stockwerkmarkierungen alle 15 Stufen'
     },
     {
-        id: 'floor_transition',
-        description: 'Übergang von Teppich zu strukturiertem Vinylboden, deutlich spürbare Kante mit Metallschiene'
+        id: 'eingangsbereich_matte',
+        description: 'Großflächige Gummimatte mit erhöhten Noppen, deutlich vom glatten Steinboden unterscheidbar, signalisiert den Haupteingang'
     },
     {
-        id: 'corner_guard',
-        description: 'Abgerundete Eckenschutz aus Kunststoff, vertikal verlaufend, etwa in Schulterhöhe tastbar'
+        id: 'richtungsanzeiger',
+        description: 'Metallener Pfeil, 3cm erhaben, weist nach rechts zum Seminarraum, Oberfläche durch häufige Berührung leicht geglättet'
     },
     {
-        id: 'braille_sign',
-        description: 'Braille-Schild rechts neben der Tür, auf Griffhöhe montiert, enthält Raumnummer und Funktionsbeschreibung'
+        id: 'raumtrenner',
+        description: 'Hüfthohe Glastrennwand mit durchgehendem Holzrahmen, warme Holzoberfläche dient als Orientierungshilfe im Foyer'
     },
     {
-        id: 'textured_strip',
-        description: 'Taktiler Bodenstreifen mit Rippenstruktur, führt direkt zum Haupteingang des Gebäudeflügels'
+        id: 'notausgang_markierung',
+        description: 'Breiter Streifen mit grober Riffelung im Boden, führt diagonal zum Notausgang, deutlich spürbar durch Schuhsohlen'
     },
     {
-        id: 'column_marker',
-        description: 'Runde Säule mit strukturierter Oberfläche, markiert wichtige Kreuzungspunkte im Gang'
+        id: 'serviceschalter',
+        description: 'Niedriger Holztresen mit abgerundeten Ecken, Oberfläche leicht strukturiert, Klingel mit großem gummiertem Taster rechts'
     },
     {
-        id: 'door_handle',
-        description: 'Schwerer Metallgriff mit charakteristischer L-Form, federt beim Herunterdrücken leicht nach oben'
+        id: 'garderobe_element',
+        description: 'Wandmontierte Kleiderhaken aus Metall, gleichmäßig angeordnet, jeweils 20cm voneinander entfernt, auf Schulterhöhe'
     },
     {
-        id: 'elevator_panel',
-        description: 'Aufzugsbedienfeld mit erhöhten Tasten und Braille-Beschriftung, Summer ertönt bei Tastendruck'
+        id: 'bibliothek_eingang',
+        description: 'Schwere Holztür mit charakteristischer Schnitzerei, griffgünstige Mulde statt konventionellem Türgriff'
     },
     {
-        id: 'bench_alcove',
-        description: 'Holzbank in einer Wandnische, glatt lackierte Oberfläche, etwa kniehoch, mit Armlehnen an beiden Seiten'
+        id: 'sanitaer_wegweiser',
+        description: 'Taktiles Leitsystem mit gerillter Oberfläche, führt zu den Sanitäranlagen, Rillen verlaufen parallel zur Laufrichtung'
     },
     {
-        id: 'water_fountain',
-        description: 'Trinkbrunnen aus gebürstetem Edelstahl, Bedienknopf an der Vorderseite deutlich hervorstehend'
+        id: 'aufenthaltsraum_sofa',
+        description: 'Großes Sofa mit samtiger Stoffbezug, markiert durch einen weichen Teppich davor, erste Sitzgelegenheit im Aufenthaltsraum'
+    },
+    {
+        id: 'akustik_orientierung',
+        description: 'Wasserspiel an der Wand erzeugt konstantes Plätschern, dient als akustischer Orientierungspunkt in der Eingangshalle'
+    },
+    {
+        id: 'handlauf_kurve',
+        description: 'Ergonomisch geformter Handlauf macht eine sanfte 90-Grad-Kurve, Richtungswechsel durch taktile Markierung angekündigt'
+    },
+    {
+        id: 'rezeption_pult',
+        description: 'Rezeptionstresen mit warmer Holzoberfläche, vordere Kante abgerundet und mit Leder gepolstert, höhenverstellbar'
+    },
+    {
+        id: 'seminarraum_tuer',
+        description: 'Doppelflügelige Tür mit strukturierter Holzmaserung, beidseitig montierte Stoßgriffe mit rutschfester Gummieinlage'
+    },
+    {
+        id: 'boden_markierung',
+        description: 'Kreisförmige Metallplatte im Boden, 30cm Durchmesser, leicht erhaben, markiert wichtige Abzweigung zu den Hörsälen'
     }
 ];
 
