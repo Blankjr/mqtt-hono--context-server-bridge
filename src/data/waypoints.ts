b6851a6419d36f3cee113d0a2389a9de18bc68a1
Blankjr <blankj.me@gmail.com>
2024-12-12 15:56:28 +0100

    chore: added prefix for option to scale to multiple buildings or floors

--- src/data/waypoints.ts
+++ src/data/waypoints.ts
@@ -24,83 +24,83 @@ export interface WaypointDetails {
   
   export const waypointsByColor: ColorGroupedWaypoints = {
     "dark-blue": {
-      "H2-P4": {
+      "04.2.H2-P4": {
         visualDescription: "dunkel blauen Wegweiser nach links folgen",
         tactileDescription: "Bodenmarkierung mit erhöhten Streifen, Links abbiegen Richtung Campus"
       },
-      "H3-P2": {
+      "04.2.H3-P2": {
         visualDescription: "dunkel blauen Wegweiser in den Zwischenflur folgen",
       },
-      "H3-P3": {
+      "04.2.H3-P3": {
         visualDescription: "dunkel blauen Wegweiser in den Zwischenflur folgen",
       }
     },
     "green": {
-      "H1-P4": {
+      "04.2.H1-P4": {
         visualDescription: "grünen Wegweiser nach rechts folgen",
       },
-      "H1-P7": {
+      "04.2.H1-P7": {
         visualDescription: "grünen Wegweiser nach rechts folgen",
       },
-      "H1-P13": {
+      "04.2.H1-P13": {
         visualDescription: "grünen Wegweiser nach rechts folgen",
       },
     },
     "light-blue": {
-      "H2-P1": {
+      "04.2.H2-P1": {
         visualDescription: "hell blauen Wegweiser nach rechts folgen",
       },
-      "H3-P1": {
+      "04.2.H3-P1": {
         visualDescription: "hell blauen Wegweiser folgen",
       },
-      "H3-P3": {
+      "04.2.H3-P3": {
         visualDescription: "hell blauen Wegweiser nach rechts folgen",
       },
-      "H3-P6": {
+      "04.2.H3-P6": {
         visualDescription: "hell blauen Wegweiser folgen",
       },
     },
     "orange": {
-      "H1-P7": {
+      "04.2.H1-P7": {
         visualDescription: "orangen Wegweiser nach links folgen",
       },
-      "H1-P13": {
+      "04.2.H1-P13": {
         visualDescription: "orangen Wegweiser nach links folgen",
       },
-      "H3-P6": {
+      "04.2.H3-P6": {
         visualDescription: "orangen Wegweiser nach rechts folgen",
       }
     },
     "red": {
-      "H1-P4": {
+      "04.2.H1-P4": {
         visualDescription: "roten Wegweiser folgen",
       },
-      "H2-P1": {
+      "04.2.H2-P1": {
         visualDescription: "roten Wegweiser nach links folgen",
       },
-      "H3-P1": {
+      "04.2.H3-P1": {
         visualDescription: "roten Wegweiser folgen",
       },
-      "H3-P3": {
+      "04.2.H3-P3": {
         visualDescription: "roten Wegweiser nach links folgen",
       }
     },
     "short-green": {
-      "V1-P3": {
+      "04.2.V1-P3": {
         visualDescription: "grünen Wegweiser nach links folgen",
       },
-      "V1-P4": {
+      "04.2.V1-P4": {
         visualDescription: "grünen Wegweiser nach rechts folgen",
       },
-      "H2-P4": {
+      "04.2.H2-P4": {
         visualDescription: "grünen Wegweiser in den Zwischenflur folgen",
       }
     },
     "yellow": {
-      "H1-P7": {
+      "04.2.H1-P7": {
         visualDescription: "gelben Wegweiser in den Zwischenflur folgen",
       },
-      "H2-P4": {
+      "04.2.H2-P4": {
         visualDescription: "gelben Wegweiser nach rechts folgen",
       }
     }
