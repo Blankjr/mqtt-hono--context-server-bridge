1e2d78228a15739cbe9a29af9a930af1f3723bf2
Blankjr <blankj.me@gmail.com>
2024-12-25 22:32:59 +0100

    chore: update production url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,7 +4,7 @@ export const SERVER_CONFIG = {
     PORT: 3000,
 //     HOST: process.env.NODE_ENV === 'production' ? undefined : '0.0.0.0',
     IS_LOCAL_NETWORK: process.env.NODE_ENV === 'development',
-    PRODUCTION_URL: 'https://mqtt-hono-context-server-bridge-production.up.railway.app',
+    PRODUCTION_URL: 'https://navigation-server-production.up.railway.app/',
     CONTEXT_URL: process.env.CONTEXT_URL,
     CONTEXT_BASIC_AUTH: process.env.CONTEXT_BASIC_AUTH
   } as const;
